ee394e03c41bb38f2d620c02890cfcab1ac64d7b
nabanita <nabanita.roy@firstdata.com>
2020-04-06 20:46:48 +0100

    Updates

--- NewsAPIClientCall/constants.py
+++ NewsAPIClientCall/constants.py
@@ -1,2 +1,7 @@
-DEFAULT_LANG = "en"
+countries_mapper = {'India':'in',
+                    'Ireland':'ie',
+                    'UK':'gb',
+                    'USA':'us'}
 
+source_mapper = {'India': 'the-times-of-india',
+                 'Ireland': 'the-irish-timsses'}
--- NewsAPIClientCall/fetch_news.py
+++ NewsAPIClientCall/fetch_news.py
@@ -8,70 +8,46 @@ class FetchData:
         """""
         self.news_api = news_api
 
-    def fetch_top_headline(self, query, sources, category, country, page_size, page, lang=constants.DEFAULT_LANG):
+    def get_top_headline_by_country(self, country, page_no=1, page_size=100):
         """
-        To fetch the top headlines in news.
+                To fetch the top headlines in news by country
 
-        :param query: query term / keyword
-        :param sources: comma-separated string of sources
-        :param category: category of news
-        :param country: 2-letter ISO 3166-1 code of the country
-        :param page_size: number of results to return per page request. default = 20, maximum = 100
-        :param page: to page through if the number of resultant page_size is more than expected
-        :param lang: language, default = all languages
-        :return:
+                :param country: 2-letter ISO 3166-1 code of the country
+                :param page_size: number of results to return per page request.
+                 In API - default = 20, maximum = 100
+                :param page_no: to page through if the number of resultant page_size is more than expected
+                :return:  json object
         """
-        return self.news_api.get_top_headlines(q=query,
-                                               sources=sources,
-                                               category=category,
-                                               language=lang,
-                                               country=country,
-                                               page=page,
+        return self.news_api.get_top_headlines(country=country,
+                                               page=page_no,
                                                page_size=page_size)
 
-    def fetch_everything_by_page(self, query, q_in_title, sources,domains,
-                                 exclude_domains, from_date, to_date,
-                                 sort_by, page_size, page,
-                                 lang=constants.DEFAULT_LANG):
+    def get_all_articles_last_month(self, query, sources, page_no=1, page_size=100):
         """
-        To fetch the news sources and blogs available in the newsapi
-        :param query: query term / keyword
-        :param q_in_title: query term / keyword to search in title
-        :param sources: comma-separated string of sources
-        :param domains:
-        :param exclude_domains:
-        :param from_date:
-        :param to_date:
-        :param sort_by:
-        :param page_size: number of results to return per page request. default = 20, maximum = 100
-        :param page: to page through if the number of resultant page_size is more than expected
-        :param lang: language, default = all languages
-        :return:
+                To fetch the top headlines in news.
+
+                :param query: query term / keyword
+                :param sources: comma-separated string of sources
+                :param page_size: number of results to return per page request.
+                In API - default = 20, maximum = 100
+                :param page_no: to page through if the number of resultant page_size is more than expected
+                :return:
         """
         return self.news_api.get_everything(q=query,
-                                            qintitle=q_in_title,
                                             sources=sources,
-                                            domains=domains,
-                                            excludeDomains=exclude_domains,
-                                            from_param=from_date,
-                                            to=to_date,
-                                            language=lang,
-                                            sort_by=sort_by,
-                                            page_size=page_size,
-                                            page=page
-                                            )
+                                            page=page_no,
+                                            page_size=page_size)
 
-    def fetch_sources(self, category, country, lang=constants.DEFAULT_LANG):
+    def get_top_headline_by_source(self, sources, page_no=1, page_size=100):
         """
-        To fetch the publishers available on the newsapi
+                To fetch the top headlines in news by country
 
-        :param category: category of news
-        :param country: 2-letter ISO 3166-1 code of the country
-        :param lang: language, default = all languages
-        :return:
+                :param sources: source id in NewsAPI
+                :param page_size: number of results to return per page request.
+                 In API - default = 20, maximum = 100
+                :param page_no: to page through if the number of resultant page_size is more than expected
+                :return:  json object
         """
-        return self.news_api.get_sources(category=category,
-                                         language=lang,
-                                         country=country
-                                         )
-
+        return self.news_api.get_top_headlines(source=sources,
+                                               page=page_no,
+                                               page_size=page_size)
