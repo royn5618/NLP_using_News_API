8c348bc814bb42992af1065ce22deb7938220880
nabanita <nroy0110@gmail.com>
2020-04-10 15:50:34 +0100

    Re-structing code base

---
+++ DataProcessor/NewsAPIGetData/constants.py
@@ -0,0 +1,6 @@
+SOURCES_MAP = {'in': ['google-news-in', 'the-times-of-india', 'the-hindu'],
+               'ie': ['the-irish-times', 'rte']
+               }
+
+COUNTRY_MAP = {'in': 'India',
+               'ie': 'Ireland'}
